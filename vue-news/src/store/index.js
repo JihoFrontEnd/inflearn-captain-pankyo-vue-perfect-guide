98fa0ea363572acc461898d369d5103b499fe9e7
Jiho Kim <jiho.kim@tysystems.com>
2021-08-28 18:38:54 +0900

    [STD-134] Call api from vuex

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex';
 
+import { fetchNewsList } from '../api';
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     newsList: [],
+  },
+  mutations: {
+    SET_NEWS_LIST(state, newsList) {
+      state.newsList = newsList;
+    },
+  },
+  actions: {
+    FETCH_NEWS_LIST(context) {
+      fetchNewsList()
+        .then(response => context.commit('SET_NEWS_LIST', response.data))
+        .catch(e => console.error(e));
+    },
   },
 });
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,29 +1,15 @@
 <template>
   <div>
-    <div v-for="news in newsList" :key="news.id">
+    <div v-for="news in this.$store.state.newsList" :key="news.id">
       {{ news.title }}
     </div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api';
-
 export default {
-  data() {
-    return {
-      newsList: [],
-    };
-  },
   created() {
-    // 화살표 함수가 아닐 경우 this를 따로 바인딩 해야 한다.
-    // 화살표 함수를 사용할 경우 this를 따로 바인딩 하지 않고 사용할 수 있다.
-    let vm = this;
-    fetchNewsList()
-      .then(function(response) {
-        vm.newsList = response.data;
-      })
-      .catch(error => console.error(error))
+    this.$store.dispatch('FETCH_NEWS_LIST');
   },
 }
 </script>
