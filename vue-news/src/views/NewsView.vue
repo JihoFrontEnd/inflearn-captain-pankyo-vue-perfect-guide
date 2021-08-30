65a487abfb6f374c32cca4a542fa3a298eff208e
Jiho Kim <jiho.kim@tysystems.com>
2021-08-30 10:29:03 +0900

    [STD-140] Dynamic route

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -4,7 +4,10 @@
       <a v-bind:href="news.url">
         {{ news.title }}
       </a>
-      <small>{{ news.time_ago }} by {{ news.user }}</small>
+      <small>
+        {{ news.time_ago }} by
+        <router-link :to="`/user/${news.user}`">{{ news.user }}</router-link>
+      </small>
     </p>
     
   </div>
