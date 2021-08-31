91d4014d46004422b06374408f42133f9e64da2c
Jiho Kim <jiho@kakao.com>
2021-08-31 12:56:42 +0900

    [STD-147] Style NewsView

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,28 +1,68 @@
 <template>
   <div>
-    <p v-for="news in this.$store.state.newsList" :key="news.id">
-      <a v-bind:href="news.url">
-        {{ news.title }}
-      </a>
-      <small>
-        {{ news.time_ago }} by
-        <router-link :to="`/user/${news.user}`">
-          {{ news.user }}
-        </router-link>
-      </small>
-    </p>
-    
+    <ul class="news-list">
+      <li v-for='news in this.$store.state.newsList' :key='news.id' class="post">
+        <div class="points">
+          {{ news.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <a v-bind:href='news.url'>
+              {{ news.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            {{ news.time_ago }} by
+            <router-link :to='`/user/${news.user}`' class="link-text">
+              {{ news.user }}
+            </router-link>
+          </small>
+        </div>
+        <!-- <a v-bind:href='news.url'>
+          {{ news.title }}
+        </a>
+        <small>
+          {{ news.time_ago }} by
+          <router-link :to='`/user/${news.user}`'>
+            {{ news.user }}
+          </router-link>
+        </small> -->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   created() {
-    this.$store.dispatch("FETCH_NEWS_LIST");
+    this.$store.dispatch('FETCH_NEWS_LIST');
   },
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
