9e8f4f66d1ce6ce8d6ba20874bf0e37e20a380a1
Jiho Kim <jiho@kakao.com>
2021-08-31 13:09:22 +0900

    [STD-148] Style Ask, Jobs View

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <p v-for="ask in getAskList" :key="ask.id">
-      <router-link v-bind:to="`item/${ask.id}`">
-        {{ ask.title }}
-      </router-link>
-      <small>
-        {{ ask.time_ago }} by {{ ask.user }}
-      </small>
-    </p>
+    <ul class="news-list">
+      <li v-for='ask in getAskList' :key='ask.id' class="post">
+        <div class="points">
+          {{ ask.points }}
+        </div>
+        <div>
+          <p class="news-title">
+            <router-link v-bind:to="`item/${ask.id}`">
+              {{ ask.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ ask.time_ago }} by
+            <router-link :to='`/user/${ask.user}`' class="link-text">
+              {{ ask.user }}
+            </router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,11 +31,34 @@ export default {
     this.$store.dispatch('FETCH_ASK_LIST');
   },
   computed: {
-    ...mapGetters(["getAskList"])
+    ...mapGetters(['getAskList']),
   }
 };
 </script>
 
-<style>
-
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
 </style>
