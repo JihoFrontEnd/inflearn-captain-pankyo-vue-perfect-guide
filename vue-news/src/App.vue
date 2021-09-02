3f6013cfccb56bd7421003d8ced78df988696c8f
Jihogrammer <jiho@kakao.com>
2021-09-02 09:03:58 +0900

    [STD-149] Add ListItem Component

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -39,29 +39,4 @@ a.router-link-exact-active {
 .page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
-/* Views Style */
-.item-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.item-title {
-  margin: 0;
-}
-.item-text {
-  color: #828282;
-}
 </style>
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,32 +1,12 @@
 <template>
   <div>
-    <ul class="item-list">
-      <li v-for='news in this.$store.state.newsList' :key='news.id' class="post">
-        <div class="points">
-          {{ news.points }}
-        </div>
-        <div>
-          <p class="item-title">
-            <a v-bind:href='news.url' target="_blank">
-              {{ news.title }}
-            </a>
-          </p>
-          <small class="item-text">
-            {{ news.time_ago }} by
-            <router-link :to='`/user/${news.user}`' class="link-text">
-              {{ news.user }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <ListItem />
   </div>
 </template>
 
 <script>
+import ListItem from '../components/ListItem.vue';
 export default {
-  created() {
-    this.$store.dispatch('FETCH_NEWS_LIST');
-  },
+  components: { ListItem },
 };
 </script>
