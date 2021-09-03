ef279d691ccecbd8b02688f56d77068a27a9cec9
Jihogrammer <jiho@kakao.com>
2021-09-03 13:27:43 +0900

    [STD-167] Implement Mixin

---
+++ vue-news/src/mixins/ListMixin.js
@@ -0,0 +1,13 @@
+import bus from '../utils/bus';
+
+export default {
+  created() {
+    bus.$emit('start:spinner');
+    this.$store.dispatch('FETCH_ITEM_LIST', this.$route.name)
+      .then(() => {
+        console.log('fetched');
+        bus.$emit('end:spinner');
+      })
+      .catch((e) => console.error(e));
+  },
+};
