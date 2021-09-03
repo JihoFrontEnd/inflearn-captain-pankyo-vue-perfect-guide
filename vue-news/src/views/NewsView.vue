a4c5afb07f983e3b7e31b4bfa3645c39896585f5
Jihogrammer <jiho@kakao.com>
2021-09-03 09:21:11 +0900

    [STD-161] Add event bus

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -6,11 +6,14 @@
 
 <script>
 import ListItem from '../components/ListItem.vue';
+import bus from '../utils/bus';
 
 export default {
   components: { ListItem },
   created() {
+    bus.$emit('start:spinner');
     this.$store.dispatch('FETCH_NEWS_LIST');
+    bus.$emit('end:spinner');
   },
 };
 </script>
