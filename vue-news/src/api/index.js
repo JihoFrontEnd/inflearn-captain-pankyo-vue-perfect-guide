ba79d800f71cb1d05149b1cc2839cbdbc3dcd954
Jihogrammer <jiho@kakao.com>
2021-09-04 17:15:45 +0900

    [STD-182] Update actions, api using async & await

--- vue-news/src/api/index.js
+++ vue-news/src/api/index.js
@@ -4,17 +4,31 @@ const config = {
   baseUrl: 'https://api.hnpwa.com/v0'
 };
 
-const fetchItemList = (pageName) => axios.get(`${config.baseUrl}/${pageName}/1.json`);
-// const fetchNewsList = () => axios.get(`${config.baseUrl}/news/1.json`);
-// const fetchAskList = () => axios.get(`${config.baseUrl}/ask/1.json`);
-// const fetchJobsList = () => axios.get(`${config.baseUrl}/jobs/1.json`);
-const fetchUserInfo = (username) => axios.get(`${config.baseUrl}/user/${username}.json`);
-const fetchItemInfo = (itemid) => axios.get(`${config.baseUrl}/item/${itemid}.json`);
+async function fetchItemList(pageName) {
+  try {
+    return await axios.get(`${config.baseUrl}/${pageName}/1.json`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchUserInfo(userName) {
+  try {
+    return await axios.get(`${config.baseUrl}/user/${userName}.json`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchItemInfo(itemId) {
+  try {
+    return await axios.get(`${config.baseUrl}/item/${itemId}.json`);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export {
-  // fetchNewsList,
-  // fetchAskList,
-  // fetchJobsList,
   fetchItemList,
   fetchUserInfo,
   fetchItemInfo,
