be09363fefa113dd4de2279f9604adbaa082daaf
Jihogrammer <jiho@kakao.com>
2021-09-08 10:38:02 +0900

    [STD-194] Slots advanced

--- toy-projects/slots/README.md
+++ toy-projects/slots/README.md
@@ -1,24 +1,6 @@
 # slots
 
-## Project setup
-```
-npm install
-```
+## props와의 차이점
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+-   slots은 상위 컴포넌트가 데이터를 가지고 있다.
+-   slots은 화면만 담당할 수 있다.
--- toy-projects/slots/src/App.vue
+++ toy-projects/slots/src/App.vue
@@ -11,6 +11,31 @@
     <ul>
       <props-item v-for="item in items" :key="item" :item="item"></props-item>
     </ul>
+    <hr>
+    <ul>
+      <slot-item v-for="item in items" :key="item">{{ item }}</slot-item>
+    </ul>
+    <hr>
+    <ul>
+      <slot-item>
+        아이템 1
+      </slot-item>
+      <slot-item>
+        아이템 2
+        <button>click me</button>
+      </slot-item>
+      <slot-item>
+        <div>
+          아이템 3
+        </div>
+        <img src="./assets/endgame.png" />
+      </slot-item>
+      <slot-item>
+        <div style="color: blue; font-size: 20px">
+          아이템 4
+        </div>
+      </slot-item>
+    </ul>
   </div>
 </template>
 
