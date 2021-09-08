e1afa94a217ac8b6f49c65c32c7facf2af43e292
Jihogrammer <jiho@kakao.com>
2021-09-08 11:16:18 +0900

    [STD-198] What is Render Function

--- toy-projects/renderless/src/main.js
+++ toy-projects/renderless/src/main.js
@@ -6,17 +6,20 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
   // 1
-  render: function(createElement) {
-    return createElement(App);
-  },
+  // createElement => Hyper Script
+  // DOM을 지칭하는 용어
+  // 그래서 'h'라고 줄여서 사용하는 것이다.
+  // render: function(createElement) {
+  //   return createElement(App);
+  // },
   // 2
-  render: function(h) {
-    return h(App);
-  },
+  // render: function(h) {
+  //   return h(App);
+  // },
   // 3
-  render: (h) => {
-    return h(App);
-  },
+  // render: (h) => {
+  //   return h(App);
+  // },
   // 4
-  render: h => h(App),
+  // render: h => h(App),
 }).$mount('#app')
