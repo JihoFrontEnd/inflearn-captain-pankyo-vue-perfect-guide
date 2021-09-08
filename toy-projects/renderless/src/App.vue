e61aca073031561168302d5f445d7de4165eceb0
Jihogrammer <jiho@kakao.com>
2021-09-08 11:25:07 +0900

    [STD-199] Renderless Component

--- toy-projects/renderless/src/App.vue
+++ toy-projects/renderless/src/App.vue
@@ -2,6 +2,10 @@
   <div>
     <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
       <!-- ... -->
+      <template slot-scope="{ response, loading}">
+        <div v-if="!loading">{{ response }}</div>
+        <div v-else>loading...</div>
+      </template>
     </fetch-data>
   </div>
 </template>
--- toy-projects/renderless/src/components/FetchData.vue
+++ toy-projects/renderless/src/components/FetchData.vue
@@ -17,6 +17,7 @@ export default {
       })
       .catch(error => {
         alert('[ERROR] fetching the data', error);
+        // eslint-disable-next-line no-console
         console.log(error);
       });
   },
